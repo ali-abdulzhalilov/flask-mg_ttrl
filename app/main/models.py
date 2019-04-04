e3c586515265ad0d9dc2e6774c397a96098473b3
mark-solo <mark.an.impostor@gmail.com>
2019-04-04 16:57:10 +0300

    user notifications (part 1)

--- app/main/models.py
+++ app/main/models.py
@@ -61,6 +61,14 @@ class User(UserMixin, db.Model):
 	about_me = db.Column(db.String(140))
 	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 	
+	messages_sent = db.relationship('Message',
+		foreign_keys='Message.sender_id',
+		backref='author', lazy='dynamic')
+	messages_received = db.relationship('Message',
+		foreign_keys='Message.recipient_id',
+		backref='recipient', lazy='dynamic')
+	last_message_read_time = db.Column(db.DateTime)
+	
 	
 	followed = db.relationship(
 		'User',
@@ -103,6 +111,11 @@ class User(UserMixin, db.Model):
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
 		
+	def new_messages(self):
+		last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+		return Message.query.filter_by(recipient=self).filter(
+			Message.timestamp > last_read_time).count()
+		
 class Post(SearchableMixin, db.Model):
 	__searchable__ = ['body']
 	id = db.Column(db.Integer, primary_key=True)
@@ -115,3 +128,12 @@ class Post(SearchableMixin, db.Model):
 	def __repr__(self):
 		return '<Post {}>'.format(self.body)
 		
+class Message(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	body = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+	
+	def __repr__(self):
+		return '<Message {}>'.format(self.body)
